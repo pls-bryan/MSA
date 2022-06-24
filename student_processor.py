60595cd3d7b67da78d5e6ef0c1d667f0dda3bc04
pls-bryan <91303789+pls-bryan@users.noreply.github.com>
2022-06-24 11:37:33 -0500

    Update student_processor.py

--- student_processor.py
+++ student_processor.py
@@ -7,25 +7,24 @@ def load_students(csv_file):
     file = open(csv_file, "r")
     file_list = file.readlines()
     student_list = []
+    counter = 1
     for x in range(1, len(file_list)):
+        counter += 1
         file_split = file_list[x].split(",")
-        try:
-            student = Student(file_split[0], file_split[1], file_split[2], file_split[3], file_split[4], file_split[5])
-        except IndexError:
-            print("Data error")
-            continue
-        finally:
-            file.close()
-            student_list.append(student)
+        if len(file_split) != 6:
+            raise Exception(f"Data Error on line {counter}")
+        student = Student(file_split[0], file_split[1], file_split[2], file_split[3], file_split[4], file_split[5])    
+        student_list.append(student)
     return student_list
 
 def print_data(student):
     print(f"Name: {student.get_first_name()} {student.get_last_name()}")
+    print(f"ID: {student.get_id_number().strip()}")
     print(f"Major: {student.get_major()}")
     print(f"Credit Hours: {student.get_credit_hours()}")
     print(f"GPA: {student.get_gpa()}")
-    print(f"Grade Level: {student.get_class_level()}")
-    print(f"ID: {student.get_id_number().strip()}\n")
+    print(f"Grade Level: {student.get_class_level()}\n")
+    
     
 def main():
     load = load_students("students.csv")
